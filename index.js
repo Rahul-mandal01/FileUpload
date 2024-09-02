1db960a7f0bb10188ad60a23cb2e7f83e6be2fd1
rahul <rahulra16809999@gmail.com>
2024-09-02 15:54:08 +0530

    added basic setup and middlewares

---
+++ index.js
@@ -0,0 +1,30 @@
+// App create 
+
+const express = require('express');
+const app = express();
+
+// Find Port
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+// add middlewares
+app.use(express.json());
+const fileupload = require("express-fileupload");
+app.use(fileupload());
+
+// connect with DB
+ const db = require("./config/database");
+ db.connect();
+
+//  connect with cloud
+cloudinary  = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+// Mount api route
+const Upload = require("./routes/FileUpload");
+app.use('/api/v1/upload', Upload);
+
+// activate server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);  // server is running on port 3000
+})
