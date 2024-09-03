ab2649ba10d7f45bf50053ee900c7221d2db9adc
rahul <rahulra16809999@gmail.com>
2024-09-03 10:47:31 +0530

    added imageUpload, videoUpload & imageReducer handler

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,4 +1,5 @@
 const File = require("../models/file");
+const cloudinary = require("cloudinary").v2;
 
 // localFileUpload -> handler function 
 
@@ -25,6 +26,181 @@ exports.localFileUpload = async (req, res) => {
         })
     }catch(error){
         console.error(error);
-        res.status(500).json({ message: "Server error" });
+        res.status(500).json({ message: "Not able to upload the file on server" });
+    }
+}
+
+function isFileTypeSupported(type, supportedTypes){
+    return supportedTypes.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder,quality){
+    const options = {folder};
+    if(quality){
+        options.quality = quality;
+    }
+    options.resource_type = "auto";
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+
+
+
+// image Upload -> handler function
+exports.imageUpload = async (req, res) => {
+    try{
+            // data fetch
+            const {name, tags, email} = req.body;
+            console.log(name, tags, email);
+            const file = req.files.imageFile;
+            console.log(file);
+
+            // validation
+            const supportedTypes = ["jpg", "jpeg", "png"];
+            const fileType = file.name.split('.')[1].toLowerCase();
+            console.log("File type: ", fileType);
+
+            if(!isFileTypeSupported(fileType, supportedTypes)){
+                return res.status(400).json({
+                    success:false,
+                    message:"File format not supported"
+                })
+            }
+
+            // File formats supported
+            // Upload on cloudinary
+            const response = await uploadFileToCloudinary(file, "codehelp");
+
+            console.log(response);
+
+            // save entry in DB
+
+            const fileData = await File.create({
+                name,
+                imageUrl: response.secure_url,
+                tags,
+                email
+            })
+
+            res.json({
+                success:true,
+                message:"Image uploaded successfully",
+                imageUrl:response.secure_url
+            })
+
+    }catch(error){
+        console.error(error);
+        res.status(400).json({
+            success:false,
+            message:"Failed to upload image"
+        });
+    }
+}
+
+
+// video upload -> handler function
+
+exports.videoUpload = async (req, res) => {
+    try{
+        
+        const {name, tags, email} = req.body;
+        console.log(name, tags, email);
+
+        const file = req.files.videoFile;
+        console.log(file);
+
+        // validation
+        const supportedTypes = ["mp4", "mov"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        console.log("File type: ", fileType);
+
+        //  TODO: add a upper limit of 5mb for video
+        if(!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success:false,
+                message:"File format not supported"
+            })
+        }
+
+        
+            // File formats supported
+            // Upload on cloudinary
+            const response = await uploadFileToCloudinary(file, "codehelp");
+
+            console.log(response);
+
+            // save entry in DB
+
+            const fileData = await File.create({
+                name,
+                imageUrl: response.secure_url,
+                tags,
+                email
+            });
+
+            res.json({
+                success:true,
+                message:"video uploaded successfully",
+                videoUrl:response.secure_url
+            })
+
+    }catch(error){
+        console.error(error);
+        res.status(400).json({
+            success: false,
+            message:"Failed to upload video"
+        });
+    }
+}
+
+
+// imageSizeReducer -> Handler function
+
+exports.imageSizeReducer = async (req, res) => {
+    try{
+            // data fetch
+            const {name, tags, email} = req.body;
+            console.log(name, tags, email);
+            const file = req.files.imageFile;
+            console.log(file);
+
+            // validation
+            const supportedTypes = ["jpg", "jpeg", "png"];
+            const fileType = file.name.split('.')[1].toLowerCase();
+            console.log("File type: ", fileType);
+
+            if(!isFileTypeSupported(fileType, supportedTypes)){
+                return res.status(400).json({
+                    success:false,
+                    message:"File format not supported"
+                })
+            }
+
+            // File formats supported
+            // Upload on cloudinary
+            const response = await uploadFileToCloudinary(file, "codehelp", 20);
+
+            console.log(response);
+
+            // save entry in DB
+
+            const fileData = await File.create({
+                name,
+                imageUrl: response.secure_url,
+                tags,
+                email
+            })
+
+            res.json({
+                success:true,
+                message:"Image uploaded successfully",
+                imageUrl:response.secure_url
+            })
+
+    }catch(error){
+        console.error(error);
+        res.status(400).json({
+            success: false,
+            message:"Failed to reduce image size"
+        });
     }
 }
