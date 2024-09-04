c87903c2a1dd738b2d74fdbd1cb53962d742c6bb
rahul <rahulra16809999@gmail.com>
2024-09-04 10:50:42 +0530

    added sendMail function

--- models/file.js
+++ models/file.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+let transporter = require("../config/nodemailer");
 
 
 const fileSchema = new mongoose.Schema({
@@ -14,6 +15,26 @@ const fileSchema = new mongoose.Schema({
     },
     email:{
         type:String
+    }
+})
+
+// post middleware
+fileSchema.post("save", async function(doc){
+    try{
+        console.log("DOC", doc);
+
+        // send mail
+
+        let info = await transporter.sendMail({
+            from: '"CodeHelp- by Rahul" mrrahul574@gmail.com ', // sender address
+            to: doc.email, // list of receivers
+            subject: "New File Uploaded on Cloudinary", // Subject line
+            text: `A new file named ${doc.name} has been uploaded to our platform. You can access it here: ${doc.imageUrl}`, // plain text body
+        })
+        console.log("INFO: ", info);
+    }catch(error){
+        console.error("Error saving file", error);
+        throw error;
     }
 })
 
